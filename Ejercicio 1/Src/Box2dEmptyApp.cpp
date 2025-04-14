cd1af98e17ba8f03937e99231fd24d8371f773d2
Maxomiliano <max.nebula1@gmail.com>
2025-04-14 15:23:43 -0300

    Reestructuración de carpetas

--- Ejercicio 1/Src/Box2dEmptyApp.cpp
+++ Ejercicio 1/Src/Box2dEmptyApp.cpp
@@ -65,7 +65,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
     // Creación del juego y bucle principal
     Game* Juego;
-    Juego = new Game(800, 600, "Resortes - MAVII");
+    Juego = new Game(800, 600, "Ejercicio 1");
     Juego->Loop(); // Inicia el bucle principal del juego
 
     return 0;
